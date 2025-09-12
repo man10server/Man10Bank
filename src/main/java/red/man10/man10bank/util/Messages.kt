c787857b8395a9ea6c5a56b0fda564b834699a23
Jin Morikawa <forest611328@gmail.com>
2025-09-12 23:38:45 +0900

    feat(util): プレフィックス付きsendMessageユーティリティを追加し、/man10bank出力に適用

--- src/main/java/red/man10/man10bank/util/Messages.kt
+++ src/main/java/red/man10/man10bank/util/Messages.kt
@@ -4,13 +4,13 @@ import org.bukkit.command.CommandSender
 
 /**
  * メッセージ送信ユーティリティ。
- * - 全メッセージに [Man10Bank] のプレフィックスを付与します。
+ * - 全メッセージにのプレフィックスを付与します。
  */
 object Messages {
     private const val PREFIX: String = "§f§l[§e§lMan10Bank§f§l]§f "
 
     /** 単一行にプレフィックスを付けた文字列を返します。 */
-    fun prefixed(message: String): String = PREFIX + message
+    private fun prefixed(message: String): String = PREFIX + message
 
     /** 単一行メッセージを送信します。 */
     fun send(sender: CommandSender, message: String) {
